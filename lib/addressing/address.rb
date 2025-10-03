7f1fb4c6f91ca656d67dd9bb0979688d313f9546
Robin van der Vleuten <robin@webstronauts.com>
2025-10-03 14:13:59 +0200

    Refactor Address class

--- lib/addressing/address.rb
+++ lib/addressing/address.rb
@@ -2,7 +2,13 @@
 
 module Addressing
   class Address
-    attr_reader :country_code, :administrative_area, :locality, :dependent_locality, :postal_code, :sorting_code, :address_line1, :address_line2, :address_line3, :organization, :given_name, :additional_name, :family_name, :locale
+    FIELDS = %i[
+      country_code administrative_area locality dependent_locality
+      postal_code sorting_code address_line1 address_line2 address_line3
+      organization given_name additional_name family_name locale
+    ].freeze
+
+    attr_reader(*FIELDS)
 
     def initialize(country_code: "", administrative_area: "", locality: "", dependent_locality: "", postal_code: "", sorting_code: "", address_line1: "", address_line2: "", address_line3: "", organization: "", given_name: "", additional_name: "", family_name: "", locale: "und")
       @country_code = country_code
@@ -21,92 +27,17 @@ module Addressing
       @locale = locale
     end
 
-    def with_country_code(country_code)
-      address = clone
-      address.country_code = country_code
-      address
-    end
-
-    def with_administrative_area(administrative_area)
-      address = clone
-      address.administrative_area = administrative_area
-      address
-    end
-
-    def with_locality(locality)
-      address = clone
-      address.locality = locality
-      address
-    end
-
-    def with_dependent_locality(dependent_locality)
-      address = clone
-      address.dependent_locality = dependent_locality
-      address
-    end
-
-    def with_postal_code(postal_code)
-      address = clone
-      address.postal_code = postal_code
-      address
-    end
-
-    def with_sorting_code(sorting_code)
-      address = clone
-      address.sorting_code = sorting_code
-      address
-    end
-
-    def with_address_line1(address_line1)
-      address = clone
-      address.address_line1 = address_line1
-      address
-    end
-
-    def with_address_line2(address_line2)
-      address = clone
-      address.address_line2 = address_line2
-      address
-    end
-
-    def with_address_line3(address_line3)
-      address = clone
-      address.address_line3 = address_line3
-      address
-    end
-
-    def with_organization(organization)
-      address = clone
-      address.organization = organization
-      address
-    end
-
-    def with_given_name(given_name)
-      address = clone
-      address.given_name = given_name
-      address
-    end
-
-    def with_additional_name(additional_name)
-      address = clone
-      address.additional_name = additional_name
-      address
-    end
-
-    def with_family_name(family_name)
-      address = clone
-      address.family_name = family_name
-      address
-    end
-
-    def with_locale(locale)
-      address = clone
-      address.locale = locale
-      address
+    # Define with_* methods for all fields
+    FIELDS.each do |field|
+      define_method("with_#{field}") do |value|
+        address = clone
+        address.send("#{field}=", value)
+        address
+      end
     end
 
     protected
 
-    attr_writer :country_code, :administrative_area, :locality, :dependent_locality, :postal_code, :sorting_code, :address_line1, :address_line2, :address_line3, :organization, :given_name, :additional_name, :family_name, :locale
+    attr_writer(*FIELDS)
   end
 end
